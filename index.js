7fcc7afd28725a976878fa5dca3cc47228cc36a5
Benjamin Arruebarrena <arruebarrena15@gmail.com>
2022-04-02 12:22:12 -0300

    iniciandoPrimerEjersicio

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+//let nombre = 'benjamin'
+
+//if (nombre == 'lucas') {
+//    alert('tu nobre es ' + nombre);
+//} 
+
+//else if (nombre == 'dalto') {
+  //  alert('tu nombre es dalto');
+//}
+
+//else {
+  //  alert('tu nombre es otro');
+//}
+
+
