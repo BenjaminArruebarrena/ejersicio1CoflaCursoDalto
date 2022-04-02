1e947932efa2183c5fabd6e3b2d7fe338fe2cc9c
Benjamin Arruebarrena <arruebarrena15@gmail.com>
2022-04-02 12:52:12 -0300

    dinero de cofla

--- index.js
+++ index.js
@@ -15,3 +15,32 @@
 //}
 
 
+dineroCofla = prompt('cuanto dinero tienes cofla?');
+
+if (dineroCofla >= 0.6 && dineroCofla < 1){
+    alert('comprate el elado de agua')
+}
+
+else if (dineroCofla >= 1 && dineroCofla < 1.6) {
+    alert('comprate el helado de crema')
+}
+
+else if (dineroCofla >= 1.6 && dineroCofla < 1.7) {
+    alert('comprate el helado heladix')
+}
+
+else if (dineroCofla >= 1.7 && dineroCofla < 1.8) {
+    alert('comprate el helado heladovix')
+}
+
+else if (dineroCofla >= 1.8 && dineroCofla < 2.9) {
+    alert('comprate el helado helardo')
+}
+
+else if (dineroCofla >= 2.9) {
+    alert('comprate el helado de confites o el helado de 1/4 kg')
+} else {
+    alert('lo siento no tienes suficiente dinero')
+}
+dineroRoberto = prompt('cuanto dinero tienes Roberto?');
+dineroPedro = prompt('cuanto dinero tienes pedro?');
